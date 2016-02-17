19475d57be94f2989949659f7330256044597db4
alexy <od-5@yandex.ru>
2016-02-17 13:50:22 +0300

    fix client update form

--- apps/client/forms.py
+++ apps/client/forms.py
@@ -14,6 +14,7 @@ class ClientUpdateForm(forms.ModelForm):
         widgets = {
             'user': forms.Select(attrs={'class': 'form-control hide'}),
             'city': forms.Select(attrs={'class': 'form-control'}),
+            'manager': forms.Select(attrs={'class': 'form-control'}),
             'legal_name': forms.TextInput(attrs={'class': 'form-control'}),
             'actual_name': forms.TextInput(attrs={'class': 'form-control'}),
             'inn': forms.TextInput(attrs={'class': 'form-control'}),
