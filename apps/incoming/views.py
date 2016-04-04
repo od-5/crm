35f793e3c04f3813961cb13a201d84fc1a3bc6b5
alexy <od-5@yandex.ru>
2016-04-04 10:47:55 +0300

    fix incomingclient task list

--- apps/incoming/views.py
+++ apps/incoming/views.py
@@ -274,6 +274,9 @@ def incomingtask_list(request):
     elif user.type == 2:
         qs = IncomingTask.objects.filter(manager__moderator=user)
     elif user.type == 5:
+        if user.is_leader_manager():
+            manager = Manager.objects.get(user=user)
+            qs = qs = IncomingTask.objects.filter(manager__moderator=manager.moderator)
         qs = IncomingTask.objects.filter(manager__user=user)
     else:
         qs = None
