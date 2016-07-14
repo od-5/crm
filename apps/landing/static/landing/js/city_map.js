3e94f2092277130a72a4adfae4c3af5ea1ff6f63
alexy <od-5@yandex.ru>
2016-07-14 15:13:50 +0300

    fix landing map

--- apps/landing/static/landing/js/city_map.js
+++ apps/landing/static/landing/js/city_map.js
@@ -1,6 +1,4 @@
 ymaps.ready(function () {
-
-
   var myMap;
   var url = $('#YMapsID').data('url');
   city = 'Екатеринбург';
@@ -19,7 +17,6 @@ ymaps.ready(function () {
         if (data.city_list.length) {
           var city_list = data.city_list;
           for(var i = 0, len = city_list.length; i < len; i++) {
-            console.log(city_list[i]);
             myMap.geoObjects.add(
               new ymaps.Placemark([city_list[i]['coord_y'], city_list[i]['coord_x']], {
               balloonContent: city_list[i]['name'] + ' Количество поверхностей: ' + city_list[i]['surface_count'],
@@ -28,21 +25,7 @@ ymaps.ready(function () {
             );
           }
         }
-
       });
-      //$.get(url,
-      //  function(e) {
-      //    function outputItem(item, i, e) {
-      //      myMap.geoObjects.add(
-      //        new ymaps.Placemark([item['coord_y'], item['coord_x']], {
-      //        balloonContent: item['name'] + 'Количество поверхностей: ' + item['surface_count'],
-      //        hintContent: item['name'] + 'Количество поверхностей: ' + item['surface_count']
-      //        })
-      //      );
-      //    }
-      //    e.forEach(outputItem);
-      //  }
-      //);
     }
   );
 });
